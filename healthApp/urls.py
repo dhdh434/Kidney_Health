5938beea86a5d3dd476b0cbc49b720c82bfbdc7b
David <davidshunter2@gmail.com>
2022-11-29 15:57:37 -0700

    db

--- healthApp/urls.py
+++ healthApp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import indexPageView, aboutPageView, loginPageView, bootPageView, createAccPageView
 
 urlpatterns = [
-    path("about", aboutPageView, name="about"),
-    path("login", loginPageView, name="login"),
-    path("createacc", createAccPageView, name="createAcc"),
-    path("boot", bootPageView, name="boot"),
+    path("about/", aboutPageView, name="about"),
+    path("login/", loginPageView, name="login"),
+    path("createacc/", createAccPageView, name="createAcc"),
+    path("boot/", bootPageView, name="boot"),
     path("", indexPageView, name="index"),
     
 ]
