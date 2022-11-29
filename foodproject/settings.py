4141fcb3223adc87131960ea5a3150fb6bea9097
David <davidshunter2@gmail.com>
2022-11-29 10:08:39 -0700

    database

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'foodproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kidneydb',
+        'USER': 'postgres',
+        'PASSWORD': 'Dave',
+        'HOST': 'localhost'
     }
 }
+
 
 
 # Password validation
